d52ad83dab22815691a67984f91d3eb295506af0
BumBumGame <tim555@t-online.de>
2023-01-18 15:24:13 +0100

    Untested: Updated Discorvery algroithm

--- src/discovery.lua
+++ src/discovery.lua
@@ -1,11 +1,11 @@
 local log = require "log"
 local capabilities = require "st.capabilities"
 local Driver = require "st.driver"
+local utils = require "st.utils"
 
 --local librarys
 local mDNS = require "mDNS"
 --local imports
-local http = require("httpRequests")
 
 
 local Discovery = {}
@@ -15,21 +15,6 @@ local Discovery = {}
 local function getMacFromController()
 
 end
-
---tmp--------------------------------------------------------
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
---tmp--------------------------------------------------------
 
 function Discovery.discovery_Handler(driver, _, should_continue)
 	local foundDevices = {}
@@ -41,14 +26,18 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 		end
 
 	
-	while should_continue do 
+	while should_continue() do 
 		--Searching WLED mDNS Space
-		    mDNS.get_services("_wled._tcp.local", function (deviceTable) 
+			local dnsSpace = "_wled._tcp.local"
+		    mDNS.get_services(dnsSpace, function (deviceTable) 
 			--iterate throught found devices
-			log.info("DeviceTable: " .. dump(deviceTable)) -- -->> Contains MAC!!!!! --> Table:  DeviceTable: { ["wled-996ad8.local"] = { ["ip"] = 192.168.178.61,} ,["wled-996ad8._wled._tcp.local"] = { ["info"] = { ["mac"] = e8db84996ad8,} ,["hostnames"] = { [1] = wled-996ad8.local,} ,["port"] = 80,} ,["_wled._tcp.local"] = { ["instances"] = { [1] = wled-996ad8._wled._tcp.local,} ,} ,}
-			for _,value in ipairs(deviceTable.instances) do
-			 
-			 
+			log.info("DeviceTable: " .. utils.stringify_table(deviceTable)) -- -->> Contains MAC!!!!! --> Table:  DeviceTable: { ["wled-996ad8.local"] = { ["ip"] = 192.168.178.61,} ,["wled-996ad8._wled._tcp.local"] = { ["info"] = { ["mac"] = e8db84996ad8,} ,["hostnames"] = { [1] = wled-996ad8.local,} ,["port"] = 80,} ,["_wled._tcp.local"] = { ["instances"] = { [1] = wled-996ad8._wled._tcp.local,} ,} ,}
+			for _,deviceName in ipairs(deviceTable[dnsSpace]["instances"]) do
+			 local deviceInfo = {
+				macAdress = deviceTable[deviceName]["info"]["mac"],
+				ipAdress = deviceTable[deviceTable[deviceName]["info"]["hostnames"][1]]["ip"]
+			 }
+			 log.info("DeviceInfo: " .. utils.stringify_table(deviceInfo))
 			end
 		
 		end)
