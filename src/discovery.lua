d049b3882b6d7b3f55b4302c6a2b9843ca01cc7e
Tim <tim555@t-online.de>
2023-04-08 18:20:25 +0200

    Changed Network id Generation

--- src/discovery.lua
+++ src/discovery.lua
@@ -3,6 +3,7 @@ local capabilities = require("st.capabilities")
 local Driver = require("st.driver")
 local utils = require("st.utils")
 local log = require("log")
+local socket = require("socket")
 
 --local librarys
 local mDNS = require("mDNS")
@@ -29,20 +30,21 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 		--Searching WLED mDNS Space
 			local dnsSpace = config.MDNS_SERVICE_NAME
 			
-		    mDNS.get_services(dnsSpace, function (deviceTable) 
+		    mDNS.get_services(dnsSpace, function (deviceTable)
+							log.info(utils.stringify_table(deviceTable))
 			--iterate throught found devices
         	 for _,deviceName in ipairs(deviceTable[dnsSpace]["instances"]) do
-			 
 				--filter device MDns info
-			    local macAdress = deviceTable[deviceName]["info"]["mac"]
-				local networkID = deviceName..":"..macAdress
+				local hostname = deviceTable[deviceName]["hostnames"][1]
+			    local ipAddress = deviceTable[hostname]["ip"]
+				local networkID = deviceName..":"..ipAddress
 				
 				if not knownDevices[networkID] and not foundDevices[networkID] then
 				
 					local metaData = {
 						type = "LAN",
 						device_network_id = networkID,
-						label = deviceName,
+						label = hostname,
 						profile = config.MAIN_PROFILE_NAME,
 						manufacturer = "Aircookie",
 						model = "WLED"
@@ -59,6 +61,9 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 			end
 		
 		end)
+		
+	--small sleep on end of discovery
+	socket.sleep(4)
 	end
 	
 	log.info("Stopping Discovery-------")
