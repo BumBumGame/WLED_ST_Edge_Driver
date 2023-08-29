cb2141ab1f79d886631ddbcc07c125868fcfc228
Tim <tim555@t-online.de>
2023-08-29 19:34:33 +0200

    Fixed and Tested Discovery: should_continue still stays true forever

--- src/discovery.lua
+++ src/discovery.lua
@@ -2,6 +2,7 @@
 local capabilities = require("st.capabilities")
 local Driver = require("st.driver")
 local utils = require("st.utils")
+local net_utils = require("st.net_utils")
 local log = require("log")
 local socket = require("socket")
 local mDNS = require("st.mdns")
@@ -29,22 +30,23 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 		--Searching WLED mDNS Space
 			local mdnsResponse = mDNS.discover(config.MDNS_SERVICE_NAME, config.MDNS_PROTOCOL) or {};
 
-			for nil, found in ipairs(mdnsResponse) do
+			for _, found in pairs(mdnsResponse) do
+				found = found[1] --One Step down in hierachy because of false Smartthings Plattform Documentation
 				--Security Check for Service Name, nil and IPv4
-				if found == nil or found.service_info.name ~= config.MDNS_SERVICE_NAME and net_utils.validate_ipv4_string(found.host_info.address) then
+				if found == nil or found.service_info.name ~= config.MDNS_SERVICE_NAME and not net_utils.validate_ipv4_string(found.host_info.address) then
 					goto continue
 				end
 				--Process mdns Response
 				--Create Network ID from Hostname and mDNS Service Name ('HostName:ServiceName')
-				local deviceNetworkId = found.host_info.name..config.DEVICE_ID_SPLIT_SYMBOL..found.service_info.name
+				local deviceNetworkId = found.host_info.name..config.DEVICE_ID_SPLIT_SYMBOL..found.service_info.service_type
 
 				--If Device ist not known to the hub and has not been added in the current Search, add it to the hub
-				if not knownDevices[networkID] and not foundDevices[networkID] then
+				if not knownDevices[deviceNetworkId] and not foundDevices[deviceNetworkId] then
 					--Create Metadata of device
 					local metaData = {
 						type = "LAN",
 						device_network_id = deviceNetworkId,
-						label = hostname,
+						label = found.host_info.name,
 						profile = config.MAIN_PROFILE_NAME,
 						manufacturer = "Aircookie",
 						model = "WLED"
@@ -52,11 +54,12 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 
 					--Add device with Metadata
 					log.info("Trying to create Device with: "..utils.stringify_table(metaData))
+					foundDevices[deviceNetworkId] = true
 					assert(driver:try_create_device(metaData))
-					foundDevices[networkID] = true
 				
 				else
 					log.info("Discovered already known or found Device: "..deviceNetworkId)
+					log.warn(should_continue())
 				end
 				::continue::
 			end
