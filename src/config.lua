97595b4dd69b458191723701bdd07470100768bb
BumBumGame <tim555@t-online.de>
2023-08-29 09:09:47 +0200

    Untested! Rewrote Init and Discovery Both now Rely on the internal mdns Library - Removed external mdns Library - Updated DeviceID Generation

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,10 @@
 local config = {}
 --Driver sepcific static info
-config.MDNS_SERVICE_NAME = "_wled._tcp.local"
 config.MAIN_PROFILE_NAME = "W2812BStrip"
+config.DEVICE_ID_SPLIT_SYMBOL = ":"
+
+--MDns Config
+config.MDNS_SERVICE_NAME = "_wled._tcp"
+config.MDNS_PROTOCOL = "local"
 
 return config
--- src/discovery.lua
+++ src/discovery.lua
@@ -4,6 +4,7 @@ local Driver = require("st.driver")
 local utils = require("st.utils")
 local log = require("log")
 local socket = require("socket")
+local mDNS = require("st.mdns")
 
 --local imports
 local config = require("config")
@@ -13,7 +14,7 @@ local Discovery = {}
 
 function Discovery.discovery_Handler(driver, _, should_continue)
 	log.info("Starting Discovery-------")
-
+	--tmp Variable to store found devices in this Search
 	local foundDevices = {}
 	
 	--Get known devices
@@ -26,46 +27,44 @@ function Discovery.discovery_Handler(driver, _, should_continue)
 	
 	while should_continue() do 
 		--Searching WLED mDNS Space
-			local dnsSpace = config.MDNS_SERVICE_NAME
-			
-		    mDNS.get_services(dnsSpace, function (deviceTable)
-							log.info(utils.stringify_table(deviceTable))
-			--iterate throught found devices
-        	 for _,deviceName in ipairs(deviceTable[dnsSpace]["instances"]) do
-				--filter device MDns info
-				local hostname = deviceTable[deviceName]["hostnames"][1]
-			    local ipAddress = deviceTable[hostname]["ip"]
-				local networkID = deviceName..":"..ipAddress
-				
+			local mdnsResponse = mDNS.discover(config.MDNS_SERVICE_NAME, config.MDNS_PROTOCOL) or {};
+
+			for nil, found in ipairs(mdnsResponse) do
+				--Security Check for Service Name, nil and IPv4
+				if found == nil or found.service_info.name ~= config.MDNS_SERVICE_NAME and net_utils.validate_ipv4_string(found.host_info.address) then
+					goto continue
+				end
+				--Process mdns Response
+				--Create Network ID from Hostname and mDNS Service Name ('HostName:ServiceName')
+				local deviceNetworkId = found.host_info.name..config.DEVICE_ID_SPLIT_SYMBOL..found.service_info.name
+
+				--If Device ist not known to the hub and has not been added in the current Search, add it to the hub
 				if not knownDevices[networkID] and not foundDevices[networkID] then
-				
+					--Create Metadata of device
 					local metaData = {
 						type = "LAN",
-						device_network_id = networkID,
+						device_network_id = deviceNetworkId,
 						label = hostname,
 						profile = config.MAIN_PROFILE_NAME,
 						manufacturer = "Aircookie",
 						model = "WLED"
 					}
+
+					--Add device with Metadata
+					log.info("Trying to create Device with: "..utils.stringify_table(metaData))
+					assert(driver:try_create_device(metaData))
+					foundDevices[networkID] = true
 				
-				  --Add Device
-				  log.info("Trying to create Device with: "..utils.stringify_table(metaData))
-				  assert(driver:try_create_device(metaData))
-				  foundDevices[networkID] = true
-			
 				else
-					log.info("Discovered known Device: "..networkID)
+					log.info("Discovered already known or found Device: "..deviceNetworkId)
 				end
+				::continue::
 			end
 		
-		end)
-		
-	--small sleep on end of discovery
-	socket.sleep(4)
+		--small sleep on end of discovery (To let the Plattform check whether search is still being performed)
+		socket.sleep(4)
 	end
-	
 	log.info("Stopping Discovery-------")
-	
 end
 
 return Discovery
